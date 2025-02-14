0afde3a7fbf07193a3245c24519a4796d979aea2
ghina2703 <ghina2703@gmail.com>
2025-02-14 20:31:21 +0700

    Add CreateProductFunctionalTest.java that verifies Create Product feature

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,47 +12,47 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ExtendWith(SeleniumJupiter.class)
-class HomePageFunctionalTest {
-    /**
-     * The port number assigned to the running application during test execution.
-     * Set automatically during each test run by Spring Framework's test context.
-     */
+class CreateProductFunctionalTest {
     @LocalServerPort
     private int serverPort;
 
-    /**
-     * The base URL for testing. Defaults to {@code http://localhost}.
-     */
-    @Value("${app.baseUrl: http://localhost}")
+    @Value("${app.baseUrl:http://localhost}")
     private String testBaseUrl;
 
     private String baseUrl;
 
     @BeforeEach
     void setupTest() {
-        baseUrl = String.format("%s:%d/product/list", testBaseUrl, serverPort);
+        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
     }
 
     @Test
     void pageTitle_isCorrect(ChromeDriver driver) throws Exception {
-        // Exercise
-        driver.get(baseUrl);
+        driver.get(baseUrl + "/product/create");
         String pageTitle = driver.getTitle();
-        // Verify
-        assertEquals("ADV Shop", pageTitle);
+        assertEquals("Create New Product", pageTitle);
     }
 
     @Test
-    void welcomeMessage_homePage_isCorrect(ChromeDriver driver) throws Exception {
-        // Exercise
-        driver.get(baseUrl);
-        String welcomeMessage = driver.findElement(By.tagName("h3"))
-                .getText();
-        // Verify
-        assertEquals("Welcome", welcomeMessage);
+    void userCanCreateProduct(ChromeDriver driver) {
+        driver.get(baseUrl + "/product/create");
+        WebElement nameInput = driver.findElement(By.id("nameInput"));
+        WebElement quantityInput = driver.findElement(By.id("quantityInput"));
+        WebElement submitButton = driver.findElement(By.cssSelector(".btn.btn-custom.w-100"));
+
+        nameInput.sendKeys("Indomie Seleraku");
+        quantityInput.sendKeys("18");
+        submitButton.click();
+
+        driver.get(baseUrl + "/product/list");
+        String pageSource = driver.getPageSource();
+        assertTrue(pageSource.contains("Indomie Seleraku"));
+        assertTrue(pageSource.contains("18"));
     }
+
 }
