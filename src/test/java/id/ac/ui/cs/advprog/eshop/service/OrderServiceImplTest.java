d5b48cf944ff587a16a85b013d0f7a323ac7f27f
ghina2703 <ghina2703@gmail.com>
2025-03-07 18:03:33 +0700

    [RED] Add OrderServiceImpl skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
